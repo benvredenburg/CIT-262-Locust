11bc458edd7077fe61d19595e377ab1588c83d1a
benvredenburg <vre19001@byui.edu>
2022-06-16 10:57:41 -0600

    06/16/2022

--- locustfile.py
+++ locustfile.py
@@ -1,1 +1,15 @@
-# Comment 2
+from locust import HttpUser, between, task
+
+class WebsiteUser(HttpUser):
+    wait_timme = between(5, 15)
+
+    # def on_start(self):
+    #     print("Starting up login Test...")
+
+    @task
+    def index(self):
+        self.client.get("/")
+
+    @task
+    def index(self):
+        self.client.post("/login", json={"userName": "vredenburgben@gmail.com", "password": "N7T5PpQz!"})
